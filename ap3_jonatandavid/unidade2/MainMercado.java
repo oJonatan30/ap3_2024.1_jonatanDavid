47f0ad83ac6335ac48a2db58a26e10295615322a
oJonatan30 <jonatamdavid.jd@gmail.com>
2024-09-06 21:23:55 -0300

    corrigindo (beijin no ombro😘😘)

--- ap3_jonatandavid/unidade2/MainMercado.java
+++ ap3_jonatandavid/unidade2/MainMercado.java
@@ -7,10 +7,16 @@ import unidade2.mercado_heranca.ProdutoPerecivel;
 
 public class MainMercado {
     public static void main(String[] args) {
-        Produto produto = new Produto("copo", 10);
-        ProdutoPerecivel perecivel = new ProdutoPerecivel("Queijo", 10, LocalDateTime.now());
-        System.out.println(produto);
-        System.out.println(perecivel);
+        Produto umproduto = new Produto("copo", 10);
+        Produto doisproduto = new Produto("cadeira", 20);
+        Produto tresproduto = new Produto("bolsa", 7);
+        ProdutoPerecivel umperecivel = new ProdutoPerecivel("Queijo", 10, LocalDateTime.now());
+        ProdutoPerecivel doisperecivel = new ProdutoPerecivel("Manteiga", 6, LocalDateTime.now());
+        System.out.println(umproduto);
+        System.out.println(doisproduto);
+        System.out.println(tresproduto);
+        System.out.println(umperecivel);
+        System.out.println(doisperecivel);
     }
 
 }
